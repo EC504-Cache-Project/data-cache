33cd7fc4ef4df64656a792ca82a6f326b4771cc5
Mark Barrasso <mark.barrasso@live.com>
2015-10-12 13:58:12 -0400

    changed UI. able to upload files to server. must be able to sync uploads with client using ip

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
-// required libraries ====================================
-var SocketIOFileUpload = require('socketio-file-upload'),
-    socketio = require('socket.io'),
-    express = require('express');
+// ++++++++++++++++++++++++++++++++++++++++++++
 
-// express app setup =====================================
-var app = express()
-    .use(SocketIOFileUpload.router)
-    .use("/css", express.static(__dirname + '/css'))
-    .get('/', function(req, res){
+// required libraries
+var siofu = require('socketio-file-upload');
+var dl = require('delivery');
+var express = require('express');  
+var app = express().use(siofu.router);  
+var server = require('http').createServer(app); 
+var io = require('socket.io')(server);
+var port = 8080;
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+// app setup
+app.use("/css", express.static(__dirname + '/css'));
+
+// route handler ======================
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-	})
-    .listen(9090);
+});
+
+// +++++++++++++++++++++++++++++++++++++++++++++
 
 // socket handler =====================
-// Start up Socket.IO:
-var io = socketio.listen(app);
-io.sockets.on("connection", function(socket){
+io.on('connection', function(socket){
 
-    // Make an instance of SocketIOFileUpload and listen on this socket:
-    var uploader = new SocketIOFileUpload();
-    uploader.dir = "srv/uploads";
+    // log new client connection
+    console.log('New connection:', socket.request.connection._peername);
+
+    // var delivery = dl.listen(socket);
+    // delivery.on('delivery.connect',function(delivery){
+    //     console.log('watttt');
+    // });
+
+    // log user dc events
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    /* Upload Files */
+    // listen on socket for uploads
+    var uploader = new siofu();
+    uploader.dir = __dirname + "/uploads";
     uploader.listen(socket);
 
-    // Do something when a file is saved:
+    // file uploader saved event
     uploader.on("saved", function(event){
-        console.log(event.file);
+        console.log("Client has successfully uploaded a file to the server.");
     });
 
-    // Error handler:
+    // file uploader error handler
     uploader.on("error", function(event){
-        console.log("Error from uploader", event);
+        console.log("Error uploading file: ", event);
     });
+
+});
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+// server setup =======================
+server.listen(port, function(){
+  console.log('Server is listening on port '+ port);
 });
